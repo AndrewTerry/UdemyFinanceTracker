5ae90c2ff033cf99086a81a5955c230733951834
Andrew Terry <andrew@andrewterry.com>
2022-08-12 18:35:59 +0000

    add error handling for invalid searches

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,18 @@
 class StocksController < ApplicationController
   
   def search
-    @stock = Stock.lookup(params[:stock])
-    render 'users/my_portfolio'
+    if params[:stock].present?
+      @stock = Stock.lookup(params[:stock])
+      if @stock
+        render 'users/my_portfolio'
+      else
+        flash[:alert] = "Ticker symbol must be valid"
+        redirect_to my_portfolio_path
+      end
+    else
+      flash[:alert] = "Please enter a ticker symbol"
+      redirect_to my_portfolio_path
+    end
   end
   
 end
